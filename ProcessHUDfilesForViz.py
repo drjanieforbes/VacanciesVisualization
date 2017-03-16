7d28b440e27a6a154537c5606a953ce625c15393
Dolores Jane Forbes <dolores.j.forbes@census.gov>
2017-03-16 13:05:28 -0400

    Update ProcessHUDfilesForViz.py
    Serious edits to ensure counts are correct at each level (national, state, county).  Cosmetic changes to print statements, etc.

--- ProcessHUDfilesForViz.py
+++ ProcessHUDfilesForViz.py
@@ -38,6 +38,7 @@ import os
 from glob import glob
 import pandas as pd
 import pysal as ps      # for reading .dbf files
+import csv              # for writing .csv files
 
 # ####################################################################
 # set working environment (current working directory)
@@ -64,12 +65,16 @@ def main():
 
     # initialize some variables
     numAllRecords = 0
+    numNationalRecords = 0
+    numStateRecords = 0
+    numCountyRecords = 0
+    numTractRecords = 0
     colsList = ["GEOID",
                 "month",
                 "year",
                 "AMS_RES",
                 "RES_VAC",
-                "AVG_VAC_R,
+                "AVG_VAC_R",
                 "VAC_3_RES",
                 "VAC_3_6_R",
                 "VAC_6_12R",
@@ -110,6 +115,63 @@ def main():
     totalCountyVAC_24_36R = 0
     totalCountyVAC_36_RES = 0
 
+    # open four files for output and write the headers
+    nationalFile = open('..\\HUD\\national.csv',"wb")
+    natlWriter = csv.writer(nationalFile, delimiter=',')
+    natlWriter.writerow(['Month/Year',
+                        'GEOID',
+                        'totalAllAMS_RES',
+                        'totalAllRES_VAC',
+                        'totalAllAVG_VAC_R',
+                        'totalAllVAC_3_RES',
+                        'totalAllVAC_3_6_R',
+                        'totalAllVAC_6_12R',
+                        'totalAllVAC_12_24R',
+                        'totalAllVAC_24_36R',
+                        'totalAllVAC_36_RES'])
+
+    stateFile = open('..\\HUD\\state.csv',"wb")
+    stateWriter = csv.writer(stateFile, delimiter=',')
+    stateWriter.writerow(['Month/Year',
+                        'GEOID',
+                        'totalStateAMS_RES',
+                        'totalStateRES_VAC',
+                        'totalStateAVG_VAC_R',
+                        'totalStateVAC_3_RES',
+                        'totalStateVAC_3_6_R',
+                        'totalStateVAC_6_12R',
+                        'totalStateVAC_12_24R',
+                        'totalStateVAC_24_36R',
+                        'totalStateVAC_36_RES'])
+
+    countyFile = open('..\\HUD\\county.csv',"wb")
+    countyWriter = csv.writer(countyFile, delimiter=',')
+    countyWriter.writerow(['Month/Year',
+                        'GEOID',
+                        'totalCountyAMS_RES',
+                        'totalCountyRES_VAC',
+                        'totalCountyAVG_VAC_R',
+                        'totalCountyVAC_3_RES',
+                        'totalCountyVAC_3_6_R',
+                        'totalCountyVAC_6_12R',
+                        'totalCountyVAC_12_24R',
+                        'totalCountyVAC_24_36R',
+                        'totalCountyVAC_36_RES'])
+
+    tractFile = open('..\\HUD\\tract.csv',"wb")
+    tractWriter = csv.writer(tractFile, delimiter=',')
+    tractWriter.writerow(['Month/Year',
+                        'GEOID',
+                        'tractAMS_RES',
+                        'tractRES_VAC',
+                        'tractAVG_VAC_R',
+                        'tractVAC_3_RES',
+                        'tractVAC_3_6_R',
+                        'tractVAC_6_12R',
+                        'tractVAC_12_24R',
+                        'tractVAC_24_36R',
+                        'tractVAC_36_RES'])
+
     # get list of all .dbf filenames in the directory
     fileNames = glob('..\\Shapefiles\\*.dbf')
     print(fileNames)
@@ -123,17 +185,22 @@ def main():
         # create a data dictionary
         data = dict([(var, db.by_col(var)) for var in colsList])
         
-        # convert dict to Pandas data frame
+        # convert dict to pandas data frame
         pandasDF = pd.DataFrame(data)
 
-        print("First GEOID in the file: ",pandasDF.iloc[0]['GEOID'])
+        print("First GEOID in this file: ",pandasDF.iloc[0]['GEOID'])
 
         # get current state GEOID
-        myState = pandasDF.iloc[0]['GEOID'][0:2]
+        myState = str(pandasDF.iloc[0]['GEOID'][0:2])
         print("Initial State: ",myState)
         
-        myCounty = pandasDF.iloc[0]['GEOID'][0:5]
+        # get current county GEOID
+        myCounty = str(pandasDF.iloc[0]['GEOID'][0:5])
         print("Initial County: ",myCounty)
+
+        # get current month/year for this file
+        myQtrYear = str(pandasDF.iloc[0]['month']) + "/" + str(pandasDF.iloc[0]['year'])
+        print("Month/Year: ", myQtrYear)
 
         # process each record in an individual file
         for index, row in pandasDF.iterrows():
@@ -141,6 +208,77 @@ def main():
             numAllRecords += 1
 
             # export the record to the census tract file
+            tractWriter.writerow([myQtrYear,
+                                  str(row['GEOID']),
+                                  row['AMS_RES'],
+                                  row['RES_VAC'],
+                                  row['AVG_VAC_R'],
+                                  row['VAC_3_RES'],
+                                  row['VAC_3_6_R'],
+                                  row['VAC_6_12R'],
+                                  row['VAC_12_24R'],
+                                  row['VAC_24_36R'],
+                                  row['VAC_36_RES']])
+
+            # is this a new county?
+            if (myCounty <> str(row["GEOID"][0:5])):
+                
+                # write a line to the county file for this quarter-year
+                countyWriter.writerow([myQtrYear,
+                                    myCounty,
+                                    totalCountyAMS_RES,
+                                    totalCountyRES_VAC,
+                                    totalCountyAVG_VAC_R,
+                                    totalCountyVAC_3_RES,
+                                    totalCountyVAC_3_6_R,
+                                    totalCountyVAC_6_12R,
+                                    totalCountyVAC_12_24R,
+                                    totalCountyVAC_24_36R,
+                                    totalCountyVAC_36_RES])
+
+                # number of county records processed
+                numCountyRecords += 1
+                
+                # reset the county level totals
+                totalCountyAMS_RES = 0
+                totalCountyRES_VAC = 0
+                totalCountyAVG_VAC_R = 0
+                totalCountyVAC_3_RES = 0
+                totalCountyVAC_3_6_R = 0
+                totalCountyVAC_6_12R = 0
+                totalCountyVAC_12_24R = 0
+                totalCountyVAC_24_36R = 0
+                totalCountyVAC_36_RES = 0
+                
+            # is this a new state?
+            if (myState <> str(row["GEOID"][0:2])):
+                
+                # write a line to the state file for this quarter-year
+                stateWriter.writerow([myQtrYear,
+                                    myState,
+                                    totalStateAMS_RES,
+                                    totalStateRES_VAC,
+                                    totalStateAVG_VAC_R,
+                                    totalStateVAC_3_RES,
+                                    totalStateVAC_3_6_R,
+                                    totalStateVAC_6_12R,
+                                    totalStateVAC_12_24R,
+                                    totalStateVAC_24_36R,
+                                    totalStateVAC_36_RES])
+
+                # count number of state records processed
+                numStateRecords += 1
+
+                # reset the state level totals
+                totalStateAMS_RES = 0
+                totalStateRES_VAC = 0
+                totalStateAVG_VAC_R = 0
+                totalStateVAC_3_RES = 0
+                totalStateVAC_3_6_R = 0
+                totalStateVAC_6_12R = 0
+                totalStateVAC_12_24R = 0
+                totalStateVAC_24_36R = 0
+                totalStateVAC_36_RES = 0
 
             # sum the national totals (all records within each quarter/year)
             totalAllAMS_RES += row["AMS_RES"]
@@ -164,7 +302,7 @@ def main():
             totalStateVAC_24_36R += row["VAC_24_36R"]
             totalStateVAC_36_RES += row["VAC_36_RES"]
 
-            # sum the county totals (all records within a given state-county)
+            # sum the county totals (all records within a given county in a state)
             totalCountyAMS_RES += row["AMS_RES"]
             totalCountyRES_VAC += row["RES_VAC"]
             totalCountyAVG_VAC_R += row["AVG_VAC_R"]
@@ -175,56 +313,29 @@ def main():
             totalCountyVAC_24_36R += row["VAC_24_36R"]
             totalCountyVAC_36_RES += row["VAC_36_RES"]
 
-            # get next row for comparison
-            if (index <> 73766):
-
-                # is this a new county?
-                if (myCounty <> row["GEOID"][0:5]):
-                    
-                    print("Got a new county!")
-
-                    # write the totals to the county aggregate file
-                    # NEED CODE HERE
-                    
-                    # reset the county level totals
-                    totalCountyAMS_RES = 0
-                    totalCountyRES_VAC = 0
-                    totalCountyAVG_VAC_R = 0
-                    totalCountyVAC_3_RES = 0
-                    totalCountyVAC_3_6_R = 0
-                    totalCountyVAC_6_12R = 0
-                    totalCountyVAC_12_24R = 0
-                    totalCountyVAC_24_36R = 0
-                    totalCountyVAC_36_RES = 0
-                    
-                # is this a new state?
-                if (myState <> row["GEOID"][0:2]):
-                    
-                    print("Got a new state!")
-
-                    # write the totals to the state file
-                    # NEED CODE HERE
-
-                    # reset the state level totals
-                    totalStateAMS_RES = 0
-                    totalStateRES_VAC = 0
-                    totalStateAVG_VAC_R = 0
-                    totalStateVAC_3_RES = 0
-                    totalStateVAC_3_6_R = 0
-                    totalStateVAC_6_12R = 0
-                    totalStateVAC_12_24R = 0
-                    totalStateVAC_24_36R = 0
-                    totalStateVAC_36_RES = 0
-
-            # we are done, store off the new State, County
-            myState = row["GEOID"][0:2]
+            # get ready for next row, store off the new State, County
+            myState = str(row["GEOID"][0:2])
             
-            myCounty = row["GEOID"][0:5]
+            myCounty = str(row["GEOID"][0:5])
  
         # end of all records within a file
-
+        print("The file %s is complete." % (myFile))
+        
         # write a line to the national file for this quarter-year
-        # NEED CODE HERE
+        natlWriter.writerow([myQtrYear,
+                            "01",
+                            totalAllAMS_RES,
+                            totalAllRES_VAC,
+                            totalAllAVG_VAC_R,
+                            totalAllVAC_3_RES,
+                            totalAllVAC_3_6_R,
+                            totalAllVAC_6_12R,
+                            totalAllVAC_12_24R,
+                            totalAllVAC_24_36R,
+                            totalAllVAC_36_RES])
+
+        # count number of national records processed
+        numNationalRecords += 1
 
         # reset the national totals
         totalAllAMS_RES = 0
@@ -237,8 +348,72 @@ def main():
         totalAllVAC_24_36R = 0
         totalAllVAC_36_RES = 0
 
-    # end of all files
-    print("Total number of records processed: ", numAllRecords)
+        # write a line to the state file for this quarter-year
+        stateWriter.writerow([myQtrYear,
+                            str(row["GEOID"][0:2]),
+                            totalStateAMS_RES,
+                            totalStateRES_VAC,
+                            totalStateAVG_VAC_R,
+                            totalStateVAC_3_RES,
+                            totalStateVAC_3_6_R,
+                            totalStateVAC_6_12R,
+                            totalStateVAC_12_24R,
+                            totalStateVAC_24_36R,
+                            totalStateVAC_36_RES])
+
+        # count number of state records processed
+        numStateRecords += 1
+
+        # reset the state level totals
+        totalStateAMS_RES = 0
+        totalStateRES_VAC = 0
+        totalStateAVG_VAC_R = 0
+        totalStateVAC_3_RES = 0
+        totalStateVAC_3_6_R = 0
+        totalStateVAC_6_12R = 0
+        totalStateVAC_12_24R = 0
+        totalStateVAC_24_36R = 0
+        totalStateVAC_36_RES = 0
+
+        # write the totals to the county aggregate file
+        countyWriter.writerow([myQtrYear,
+                            str(row["GEOID"][0:5]),
+                            totalCountyAMS_RES,
+                            totalCountyRES_VAC,
+                            totalCountyAVG_VAC_R,
+                            totalCountyVAC_3_RES,
+                            totalCountyVAC_3_6_R,
+                            totalCountyVAC_6_12R,
+                            totalCountyVAC_12_24R,
+                            totalCountyVAC_24_36R,
+                            totalCountyVAC_36_RES])
+
+        # count number of state records processed
+        numCountyRecords += 1
+        
+        # reset the county level totals
+        totalCountyAMS_RES = 0
+        totalCountyRES_VAC = 0
+        totalCountyAVG_VAC_R = 0
+        totalCountyVAC_3_RES = 0
+        totalCountyVAC_3_6_R = 0
+        totalCountyVAC_6_12R = 0
+        totalCountyVAC_12_24R = 0
+        totalCountyVAC_24_36R = 0
+        totalCountyVAC_36_RES = 0
+
+    # end of all input files
+    print("Total number of records processed: %i" % (numAllRecords))
+    print("Number of national records: %i" % (numNationalRecords))
+    print("Number of state records: %i" % (numStateRecords))
+    print("Number of county records: %i" % (numCountyRecords))
+    print("Number of tract records: %i" % (numTractRecords))
+
+    # close all files
+    tractFile.close()
+    countyFile.close()
+    stateFile.close()
+    nationalFile.close()
 
 main()
     
