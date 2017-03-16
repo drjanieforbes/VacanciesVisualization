403dc1c244d8d624b4a19a668cc9d284b1960fa3
Dolores Jane Forbes <dolores.j.forbes@census.gov>
2017-03-16 18:50:12 -0400

    Add files via upload
    R script to convert HUD .dbf files to .csv using rio package

---
+++ dbf2csv.R
@@ -0,0 +1,64 @@
+# ##################################################################
+# Author:  Dolores Jane Forbes
+#
+# Date:  03/16/2017
+#
+# Email:  dolores.j.forbes@census.gov
+#
+# File: dbf2csv.R
+#
+# Branch:  Geographic Research & Innovation Staff/Geography
+#
+# ##################################################################
+#
+# Read .dbf files and export as .csv files
+#
+# ##################################################################
+
+# ##################################################################
+# load libraries
+# ##################################################################
+
+library(rio)
+
+# ##################################################################
+# check to see if working directory has already been set
+# ##################################################################
+
+# version for on site work
+#if(!getwd() == "T:/$$JSL/Janie/Private/VacantHouses") {
+#	oldwd = getwd()
+#	setwd("T:/$$JSL/Janie/Private/VacantHouses")
+#}
+
+# version for telework site (home)
+if(!getwd() == "C:/CensusProjs/HUDData/VacantHouses") {
+	oldwd = getwd()
+	setwd("C:/CensusProjs/HUDData/VacantHouses")
+}
+
+# ##################################################################
+# list all HUD files
+# ##################################################################
+
+if(!exists("filenames")) {
+
+	filenames <- list.files("Shapefiles",pattern="*Data.dbf",
+		full.names=TRUE)
+
+}
+
+# ##################################################################
+# read each import file and export as .csv file
+# ##################################################################
+
+for (i in 1:length(filenames)) {
+
+	x <- import(filenames[i])
+	export(x, gsub(" ","",paste(substr(filenames[i], 1, 33),".csv")))
+}
+
+# it really was this easy ...
+
+
+
